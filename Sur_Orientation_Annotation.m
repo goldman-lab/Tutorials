7e64ce7e68536ee1240e533ef7bda667cc77e25a
msgoldman <47397664+msgoldman@users.noreply.github.com>
2019-10-25 12:28:25 -0700

    Add files via upload

---
+++ Sur_Orientation_Annotation.m
@@ -0,0 +1,24 @@
+%% Annotation of extracellular spike orientation tuning data:
+
+%% Response of  a V1 neuron to drifting full-field gratings.
+
+
+%% Dimension 1: Stimulus
+
+%% 18 conditions:  	1-16 are stimulus direction 0-337.5 in 22.5 degree increments
+%%          		17-18 are blank condition with uniform gray screen of same average luminance as grating
+
+
+%% Dimension 2: Time
+
+%% 3.5 seconds, sampled every msec (3500 time points)
+%% spike = 1; no spike = 0
+%% t = 0: stimulus on stationary
+%% t = 500: stimulus begins drifting
+%% t = 2500: stimulus off
+
+
+
+%% Dimension 3: Trials
+
+%% Repetitions of stimulus conditions (trials; n = 30)
